269c265c623c15a8bef0ec910cfb48f934bf3a61
RoyBoy <royboy0292@gmail.com>
2015-12-02 22:17:03 -0700

    Added Folders in empty web application solution thats includes Pi to the Nth and Fibonacci Sequence

---
+++ NumbersSolution/NumbersSolution/Fibonacci Sequence/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Fibonacci_Sequence
+{
+    class Program
+    {
+
+       static string userInput = "";
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Choose a number to run program. \n\n\n  1. Fibonacci Sequence \n\n\n type 'exit' to close application \n ");
+
+            while (userInput.ToUpper() != "EXIT")
+            {
+              userInput =  Console.ReadLine();
+
+                if (userInput == "1")
+                {
+                    runFibonacci();
+
+                }
+            }
+        }
+
+        static void runFibonacci()
+        {
+            // Fibonacci Sequence -Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.
+
+            int numberEntered;
+            string fibonacciNumberEntered = "";
+
+            Console.WriteLine("Enter a number to show a Fibonacci sequence to that Nth number \n\n");
+
+            while (userInput.ToUpper() != "EXIT")
+            {
+                userInput = Console.ReadLine();
+
+                try
+                {
+                    numberEntered = int.Parse(userInput);
+
+                    int[] fibSequenceArray = new int[numberEntered];
+
+                    for (int i = 0; i < fibSequenceArray.Length; i++)
+                    {
+                        if (i == 0)
+                        {
+                            fibSequenceArray[0] = 0;
+                        }
+                        if (i == 1)
+                        {
+                            fibSequenceArray[1] = 1;
+                        }
+                        if (i >= 2)
+                        {
+                            fibSequenceArray[i] = (fibSequenceArray[i - 1] + fibSequenceArray[i - 2]);
+                        }
+                    }
+
+
+                    Console.WriteLine("\nYour Sequence:\n\n");
+
+                    for (int j = 0; j < fibSequenceArray.Length; j++)
+                    {
+                        Console.Write(fibSequenceArray[j].ToString() + ' ');
+                    }
+                    Console.WriteLine("\n");
+                }
+                catch
+                {
+                    if (userInput.ToUpper() == "EXIT")
+                    {
+                        Environment.Exit(0);
+                    }
+
+                    Console.WriteLine("\nThat's not a valid number dummy, try again\n");
+                }
+
+
+                
+
+            }
+
+        }
+
+
+    }
+}
---
+++ NumbersSolution/NumbersSolution/PiToTheNth/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace PiToNthConsole
+{
+    class Program
+    {
+        // objective: find PI to the Nth Digit - Enter a number and have the program generate PI 
+        // up to that many decimal places. Keep a limit to how far the program will go. 
+
+        const double piNum = System.Math.PI;
+        static string inputResult = "";
+        static int decimalLength = 0;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Greetings traveler.\n\nEnter a number to generate a PI number with that many decimal places.\n");
+            Console.WriteLine("Type derp to exit.\n");
+
+           while (inputResult != "derp")
+           {
+             inputResult = Console.ReadLine();
+
+             if (checkValidInput())
+             {
+
+                 //the following can only handle 15 decimal places
+                // Console.WriteLine("\nBehold! Your Pi!\n");
+                //   Console.WriteLine(Math.Round(piNum, decimalLength));
+
+                 
+                 //the following is a custom class that can handle any size decimal 
+                 BigNumber x = new BigNumber(decimalLength);
+                 BigNumber y = new BigNumber(decimalLength);
+                 x.ArcTan(16, 5);
+                 y.ArcTan(4, 239);
+                 x.Subtract(y);
+                 Console.WriteLine(x.Print());
+             }
+           }
+
+           Environment.Exit(0);
+        }
+
+        protected static void showNonValidMessage()
+        {
+            Console.WriteLine("\nit looks like you didn't enter a valid number.\n\n 'Round these parts, we read and follow instructions.\n\nTryAgain\n");
+        }
+
+        protected static bool checkValidInput()
+        {
+            bool returnVal = false;
+
+            try
+            {
+                decimalLength = int.Parse(inputResult);
+                returnVal = true;
+            }
+            catch
+            {
+                //user didn't enter a valid number
+                showNonValidMessage();
+            }
+
+            return returnVal;
+            
+        }
+
+    }
+}
